c37c0eb15b609e520d93a7ea611e164a3067cdec
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-07-13 13:36:00 +1000

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DataService } from '../data/data';
+import { TableData } from '../data/table';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgFor, NgIf, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Patient Record';
+  tableData: TableData | undefined;
+  constructor(private data: DataService) {
+    this.getData();
+  }
+
+  async getData() {
+    this.tableData = await this.data.getTableData();
+    return this.tableData;
+  }
+}
